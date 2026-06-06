9ddaa4317807edc7d5f6adac1050ae8fe78a67c4
tuuhin <tuhinbhowmick2513@gmail.com>
2026-06-06 12:53:25 +0530

    Running the bluetooth flow on IO dispatcher Bluetooth isActive flow should be running on the io dispatcher, it wasn't creating a lot of pressure on main thread but still

--- composeApp/src/androidMain/kotlin/com/sam/bluepad/data/bluetooth/BluetoothStatusProviderImpl.android.kt
+++ composeApp/src/androidMain/kotlin/com/sam/bluepad/data/bluetooth/BluetoothStatusProviderImpl.android.kt
@@ -9,40 +9,42 @@ import android.content.IntentFilter
 import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
 import com.sam.bluepad.domain.bluetooth.BluetoothStateProvider
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
 
 actual class BluetoothStateProviderImpl(private val context: Context) : BluetoothStateProvider {
 
-	private val _btManager by lazy { context.getSystemService<BluetoothManager>() }
+    private val _btManager by lazy { context.getSystemService<BluetoothManager>() }
 
-	override val isBtActive: Boolean
-		get() = _btManager?.adapter?.isEnabled == true
+    override val isBtActive: Boolean
+        get() = _btManager?.adapter?.isEnabled == true
 
-	override val bluetoothStatusFlow: Flow<Boolean>
-		get() = callbackFlow {
+    override val bluetoothStatusFlow: Flow<Boolean>
+        get() = callbackFlow {
 
-			trySend(isBtActive)
+            trySend(isBtActive)
 
-			val receiver = object : BroadcastReceiver() {
-				override fun onReceive(context: Context?, intent: Intent?) {
-					if (intent == null || intent.action != BluetoothAdapter.ACTION_STATE_CHANGED) return
-					val btState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)
-					trySend(btState == BluetoothAdapter.STATE_ON)
-				}
-			}
+            val receiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    if (intent == null || intent.action != BluetoothAdapter.ACTION_STATE_CHANGED) return
+                    val btState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)
+                    trySend(btState == BluetoothAdapter.STATE_ON)
+                }
+            }
 
-			val intentFilter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+            val intentFilter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
 
-			ContextCompat.registerReceiver(
-				context,
-				receiver,
-				intentFilter,
-				ContextCompat.RECEIVER_EXPORTED
-			)
-			awaitClose {
-				context.unregisterReceiver(receiver)
-			}
-		}
+            ContextCompat.registerReceiver(
+                context,
+                receiver,
+                intentFilter,
+                ContextCompat.RECEIVER_EXPORTED,
+            )
+            awaitClose {
+                context.unregisterReceiver(receiver)
+            }
+        }.flowOn(Dispatchers.IO)
 }
--- composeApp/src/jvmMain/kotlin/com/sam/bluepad/data/bluetooth/BluetoothStatusProviderImpl.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/sam/bluepad/data/bluetooth/BluetoothStatusProviderImpl.jvm.kt
@@ -3,9 +3,12 @@ package com.sam.bluepad.data.bluetooth
 import co.touchlab.kermit.Logger
 import com.sam.bluepad.domain.bluetooth.BluetoothStateProvider
 import com.sam.bt_common.platform.PlatformBTInfoProvider
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 
 private const val TAG = "BluetoothStatusProvider"
 
@@ -19,7 +22,12 @@ actual class BluetoothStateProviderImpl : BluetoothStateProvider {
         get() = callbackFlow {
             val provider = PlatformBTInfoProvider()
             provider.use { provider ->
-                trySend(provider.isBluetoothActive())
+
+                // initial bluetooth state
+                launch(Dispatchers.IO) {
+                    trySend(provider.isBluetoothActive())
+                }
+
                 Logger.d(tag = TAG) { "BLUETOOTH STATE SEND" }
 
                 val handle = provider.registerCallback { state ->
@@ -31,5 +39,5 @@ actual class BluetoothStateProviderImpl : BluetoothStateProvider {
                     Logger.d(tag = TAG) { "BLUETOOTH CALLBACK UNREGISTERED" }
                 }
             }
-        }
+        }.flowOn(Dispatchers.IO)
 }
