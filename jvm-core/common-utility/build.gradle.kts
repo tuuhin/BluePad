a4d015d2ee858d7c58112702e22cd3b3e3fc706d
tuuhin <tuhinbhowmick2513@gmail.com>
2026-07-31 17:11:57 +0530

    Refactor font provider and update typography handling Rename `NativePlatformFontProvider` to `PlatformFontProviderImpl` and update package structure to `.theme`.

--- jvm-core/common-utility/build.gradle.kts
+++ jvm-core/common-utility/build.gradle.kts
@@ -3,31 +3,24 @@ plugins {
     alias(libs.plugins.nucleus.nna)
 }
 
-val hostOs: String = System.getProperty("os.name")
-val hostTarget = when {
-    hostOs == "Linux" -> "linuxX64"
-    hostOs == "Mac OS X" -> "macosArm64"
-    hostOs.startsWith("Windows") -> "mingwX64"
-    else -> error("Unsupported host OS: $hostOs")
-}
-
-// env will get precedence over gradle property
-val envNativeBuildType = providers.environmentVariable("NATIVE_BUILD_TYPE_RELEASE")
-val propertiesBuildType = providers.gradleProperty("cmake.buildTypeRelease")
-
 kotlin {
 
     jvmToolchain(22)
 
-    when (hostTarget) {
-        "mingwX64" -> mingwX64()
-        "macosArm64" -> macosArm64()
-        "linuxX64" -> linuxX64()
+    val os = org.gradle.internal.os.OperatingSystem.current()
+    when {
+        os.isWindows -> mingwX64()
+        os.isMacOsX -> macosArm64()
+        os.isLinux -> linuxX64()
+        else -> throw GradleException("Invalid Desktop target")
     }
 
     jvm()
 
     sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kermit)
+        }
         jvmTest.dependencies {
             implementation(libs.kotlin.test)
             implementation(libs.assertk)
@@ -43,12 +36,11 @@ kotlin {
 kotlinNativeExport {
     nativeLibName = "commonUtility"
     nativePackage = "com.sam.bluepad.platform.common_utils"
-// env will get precedence over gradle property
-    val envTypeIsRelease = envNativeBuildType.getOrElse("false")
-        .toBoolean()
-
-    val isPropertyTypeRelease = propertiesBuildType.getOrElse("false")
-        .toBoolean()
+    // env will get precedence over gradle property
+    val envNativeBuildType = providers.environmentVariable("NATIVE_BUILD_TYPE_RELEASE")
+    val propertiesBuildType = providers.gradleProperty("cmake.buildTypeRelease")
+    val envTypeIsRelease = envNativeBuildType.getOrElse("false").toBoolean()
+    val isPropertyTypeRelease = propertiesBuildType.getOrElse("false").toBoolean()
 
     buildType = if (envTypeIsRelease || isPropertyTypeRelease) "debug" else "release"
 }
